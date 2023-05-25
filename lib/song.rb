e3b90b1f34f46ec21923e77021e1e13b597e8c56
ruperthnyagesoa <ruperthnyagesoa@gmail.com>
2023-05-25 10:24:35 +0300

    Completed

--- lib/song.rb
+++ lib/song.rb
@@ -2,9 +2,40 @@ class Song
 
   attr_accessor :name, :album
 
-  def initialize(name:, album:)
+  def initialize(name:, album:, id: nil)
     @name = name
     @album = album
+    @id = id
   end
 
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS songs(
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      album TEXT
+    )
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = <<-SQL
+    INSERT INTO songs(name, album) VALUES(?, ?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.album)
+ end
+
+ def self.create(name: name, album: album)
+  song.save
 end
+
+
+end
+
+Song.create(name: "Hello", album: "25")
+
+song.name
+
+song.album
